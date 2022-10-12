4b1c7766adeec0db11b59a25f20e4735ce4d1875
clark726 <clark256412@gmail.com>
2022-10-12 15:22:15 +0800

    商品資訊

--- src/main/java/com/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/order/service/impl/OrderServiceImpl.java
@@ -2,10 +2,8 @@ package com.order.service.impl;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import com.order.model.OrderDAO;
 import com.order.model.OrderJNDI;
@@ -31,9 +29,9 @@ public class OrderServiceImpl implements OrderService {
 	public OrderVO getOrderByOrderId(OrderVO orderVO) {
 		String account = orderVO.getAccount();
 		Integer order_id = orderVO.getOrder_id();
-//搜尋沒有值		
+		//搜尋沒有值		
 		OrderVO backOrderVO = orderDao.getOrderByOrderId(account, order_id);
-		
+
 		if (backOrderVO.getMember_id() == null) {
 			orderVO.setSuccessful(false);
 			orderVO.setMessage("沒有此訂單");
@@ -42,7 +40,7 @@ public class OrderServiceImpl implements OrderService {
 			backOrderVO.setSuccessful(true);
 			return backOrderVO;
 		}
-		
+
 	}
 
 	@Override
@@ -53,43 +51,37 @@ public class OrderServiceImpl implements OrderService {
 	@Override
 	public boolean insert(OrderVO obj) {
 		List<OrderSmallVO> list = obj.getOrderSmallVO();
-		
-		
-		//以storeId當成key做分類 
-		Map<Integer,List<OrderSmallVO>> map = new HashMap();
-		for(int x=0;x<list.size();x++) {
+
+		// 以storeId當成key做分類
+		Map<Integer, List<OrderSmallVO>> map = new HashMap();
+		for (int x = 0; x < list.size(); x++) {
 			OrderSmallVO orderSmallVO = list.get(x);
-			if(map.containsKey(orderSmallVO.getStoreId())){
+			if (map.containsKey(orderSmallVO.getStoreId())) {
 				ArrayList<OrderSmallVO> newlist2 = (ArrayList<OrderSmallVO>) map.get(orderSmallVO.getStoreId());
 				newlist2.add(orderSmallVO);
-			}else {
+			} else {
 				List<OrderSmallVO> newlist = new ArrayList<>();
 				newlist.add(orderSmallVO);
 				map.put(orderSmallVO.getStoreId(), newlist);
 			}
 		}
-		
-		//計算價錢加總
-		Map<Integer, Integer> mapPrice = new HashMap<Integer, Integer>();
-			Iterator<Integer> iter = map.keySet().iterator();
-			while(iter.hasNext()) {
-				Integer key = iter.next();
-				List<OrderSmallVO> vo = map.get(key);
-				for(int j = 0; j < vo.size(); j++) {
-					if(mapPrice.containsKey(j)) {
 
-					}else {
-						mapPrice.put(j, vo.get(j).getAllPrice());
-					}
+		for (Map.Entry<Integer, List<OrderSmallVO>> entry : map.entrySet()) {
+			int total = 0;
+			for (int x = 0; x < entry.getValue().size(); x++) {
+				total += entry.getValue().get(x).getAllPrice();
+				if (x == entry.getValue().size() - 1) {
+					entry.getValue().get(0).setAllPrice(total);
 				}
 			}
-			
-		for(Integer key : map.keySet()) {
-			obj.setStore_id(key);
-//			obj.setPrice(key);
-//			orderDao.insert(obj);
 		}
-		
+
+		for (Map.Entry<Integer, List<OrderSmallVO>> entry : map.entrySet()) {
+			obj.setStore_id(entry.getKey());
+			obj.setPrice(entry.getValue().get(0).getAllPrice());
+
+		}
+
 		return false;
 	}
 
