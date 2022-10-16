b8f86fb7c9ee628a12b9f0098a326646e129fa0b
MSI\keith <skjisgood@gmail.com>
2022-10-16 17:03:01 +0800

    session

--- src/main/java/com/forum/model/ForumServlet.java
+++ src/main/java/com/forum/model/ForumServlet.java
@@ -12,43 +12,48 @@ import com.forum.model.*;
 @WebServlet("/ForumServlet")
 public class ForumServlet extends HttpServlet {
 
-	public void doGet(HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException {
-		doPost(req, res);
-	}
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        doPost(req, res);
+    }
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException {
-		req.setCharacterEncoding("UTF-8");
-		res.setCharacterEncoding("UTF-8");
-		String action = req.getParameter("action");
-		
-		if(action != null) {
-			switch (action) {
-			case "watchOneForum": 
-				doPostOne(req, res);
-				break;
-			default:
-				System.out.println("action "+action);
-			}
-		}else {
-			res.sendRedirect("/TGA103G1/fornt-end/forum/forum.jsp");
-		}
-	}
-	
-	public void doPostOne(HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException {
-		Integer page = Integer.parseInt(req.getParameter("page"));
-		ForumJNDI jndi = new ForumJNDI();
-		ForumVO content = jndi.get(page);
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        res.setCharacterEncoding("UTF-8");
+        String action = req.getParameter("action");
+
+        if (action != null) {
+            switch (action) {
+                case "watchOneForum":
+                    doPostOne(req, res);
+                    break;
+                default:
+                    System.out.println("action " + action);
+            }
+        } else {
+            res.sendRedirect("/TGA103G1/fornt-end/forum/forum.jsp");
+        }
+    }
+
+    public void doPostOne(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        Integer page = Integer.parseInt(req.getParameter("page"));
+        ForumJNDI jndi = new ForumJNDI();
+        ForumVO content = jndi.get(page);
 //		System.out.println(content.getContent());
-		System.out.println("=====1");
-		req.setAttribute("forumContentMemberId", jndi.findMemberByForumId(page));
-		System.out.println("=====2");
-		req.setAttribute("vo", content);
-		req.setAttribute("forumMessage", jndi.getAllByForumMessage(page));
-		System.out.println("=====3");
-		req.getRequestDispatcher("/fornt-end/forum/Content.jsp").forward(req, res);
-	}
-	
+        HttpSession session = req.getSession();
+        if (session.getAttribute("forumContentMemberId") == null) {
+            session.setAttribute("forumContentMemberId", jndi.findMemberByForumId(page));
+            session.setAttribute("vo", content);
+            session.setAttribute("forumMessage", jndi.getAllByForumMessage(page));
+            req.getRequestDispatcher("/fornt-end/forum/Content.jsp").forward(req, res);
+        } else {
+            session.getAttribute("forumContentMemberId");
+            session.getAttribute("vo");
+            session.getAttribute("forumMessage");
+
+            req.getRequestDispatcher("/fornt-end/forum/Content.jsp").forward(req, res);
+        }
+    }
 }
