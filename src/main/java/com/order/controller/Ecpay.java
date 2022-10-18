bba0a8bc638811a38b2daf90b393e903d0c7cceb
clark726 <clark256412@gmail.com>
2022-10-18 22:03:12 +0800

    綠界傳值

--- src/main/java/com/order/controller/Ecpay.java
+++ src/main/java/com/order/controller/Ecpay.java
@@ -16,32 +16,21 @@ import com.order.model.OrderVO;
 import com.order.service.OrderService;
 import com.order.service.impl.OrderServiceImpl;
 
-@WebServlet("/AddOrder")
-public class AddOrder extends HttpServlet {
+@WebServlet("/Ecpay")
+public class Ecpay extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Gson _gson = new Gson();
 	private OrderService orderSvc = new OrderServiceImpl();
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		OrderVO orderVO = _gson.fromJson(request.getReader().readLine(), OrderVO.class);
-		MemberVO memberVO = (MemberVO) request.getSession().getAttribute("userid");
-		Integer memberId = memberVO.getMember_id();
-		orderVO.setMember_id(memberId);
-		OrderVO backVO = orderSvc.insert(orderVO);
-		if (backVO.getMessage() != null) {
-			orderVO.setSuccessful(true);
-		} else {
-			orderVO.setSuccessful(false);
-			orderVO.setMessage("請聯絡店家");
-		}
-		response.setContentType("application/json");
-		try (PrintWriter pw = response.getWriter()) {
-			pw.print(new GsonBuilder().create().toJson(backVO));
-		} catch (Exception e) {
+		OrderVO backVo = orderSvc.ecPay(orderVO);
+		try(PrintWriter pw = response.getWriter()){
+			pw.print(new GsonBuilder().create().toJson(backVo));
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
--- src/main/java/com/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/order/service/impl/OrderServiceImpl.java
@@ -1,9 +1,13 @@
 package com.order.service.impl;
 
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -15,10 +19,17 @@ import com.order.service.OrderService;
 import com.order_detail.model.Order_detailDAO;
 import com.order_detail.model.Order_detailJNDI;
 
+import ecpay.payment.integration.AllInOne;
+import ecpay.payment.integration.domain.AioCheckOutALL;
+import ecpay.payment.integration.ecpayOperator.EcpayFunction;
+import net.bytebuddy.asm.Advice.Return;
+
 public class OrderServiceImpl implements OrderService {
 
 	private OrderDAO orderDao;
 	private Order_detailDAO order_detailDAO;
+	public static AllInOne allInOne;
+
 	public OrderServiceImpl() {
 
 		orderDao = new OrderJNDI();
@@ -34,7 +45,7 @@ public class OrderServiceImpl implements OrderService {
 	public OrderVO getOrderByOrderId(OrderVO orderVO) {
 		String account = orderVO.getAccount();
 		Integer order_id = orderVO.getOrder_id();
-		//搜尋沒有值		
+		// 搜尋沒有值
 		OrderVO backOrderVO = orderDao.getOrderByOrderId(account, order_id);
 
 		if (backOrderVO.getMember_id() == null) {
@@ -54,7 +65,7 @@ public class OrderServiceImpl implements OrderService {
 	}
 
 	@Override
-	public boolean insert(OrderVO obj) {
+	public OrderVO insert(OrderVO obj) throws UnsupportedEncodingException {
 		List<OrderSmallVO> list = obj.getOrderSmallVO();
 
 		// 以storeId當成key做分類
@@ -70,10 +81,10 @@ public class OrderServiceImpl implements OrderService {
 //				map.put(orderSmallVO.getStoreId(), newlist);
 //			}
 //		}
-		//以stream API方式
-		map = list.stream().collect(Collectors.groupingBy(OrderSmallVO :: getStoreId ));
-		
-		//算出商品總和放到第一個商品的allprice
+		// 以stream API方式
+		map = list.stream().collect(Collectors.groupingBy(OrderSmallVO::getStoreId));
+
+		// 算出商品總和放到第一個商品的allprice
 		for (Map.Entry<Integer, List<OrderSmallVO>> entry : map.entrySet()) {
 			int total = 0;
 			for (int x = 0; x < entry.getValue().size(); x++) {
@@ -87,14 +98,42 @@ public class OrderServiceImpl implements OrderService {
 		for (Map.Entry<Integer, List<OrderSmallVO>> entry : map.entrySet()) {
 			obj.setStore_id(entry.getKey());
 			obj.setPrice(entry.getValue().get(0).getAllPrice());
-			 orderId =  orderDao.insert(obj);
-			for(int i = 0; i < entry.getValue().size(); i++) {
-			Integer amount = entry.getValue().get(i).getCount();
-			Integer productId = entry.getValue().get(i).getProductId();
-				order_detailDAO.insert(orderId , productId , amount);
+			orderId = orderDao.insert(obj);
+			for (int i = 0; i < entry.getValue().size(); i++) {
+				Integer amount = entry.getValue().get(i).getCount();
+				Integer productId = entry.getValue().get(i).getProductId();
+				order_detailDAO.insert(orderId, productId, amount);
 			}
 		}
-		return orderId != 0;
+//		----------------------------綠界------------------------------------
+		List<OrderSmallVO> orderSmallVOs2 = obj.getOrderSmallVO();
+		// 綠界取出商品名稱
+		List<String> productName = orderSmallVOs2.stream().map(e->e.getName()).collect(Collectors.toList());
+		// 為了配合綠界
+		Optional<String> reduce = productName.stream().reduce((String acc, String curr) -> {
+			return acc + "#" + curr;
+		});
+		String name = reduce.get();
+		// 時間
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMddHHmm");
+		String orderDate = sdf.format(new Date());
+		String orderDate2 = sdf2.format(new Date());
+		allInOne = new AllInOne("");
+		AioCheckOutALL aCheckOut = new AioCheckOutALL();
+		aCheckOut.setMerchantTradeNo(orderDate2 + "tga103");
+		aCheckOut.setMerchantTradeDate(orderDate);
+		aCheckOut.setTotalAmount(obj.getPrice() + "");
+		aCheckOut.setTradeDesc("test");
+		aCheckOut.setItemName(name);
+		aCheckOut.setClientBackURL("http://localhost:8080/TGA103G1/main.html");
+		aCheckOut.setReturnURL("http://localhost:8080/TGA103G1/main.html");
+		aCheckOut.setNeedExtraPaidInfo("N");
+
+		String checkValue = EcpayFunction.genCheckMacValue("pwFHCqoQZGmho4w6", "EkRm7iFT261dpevs", aCheckOut);
+		obj.setAio(aCheckOut);
+		obj.setMessage(checkValue);
+		return obj;
 	}
 
 	@Override
@@ -105,6 +144,36 @@ public class OrderServiceImpl implements OrderService {
 	@Override
 	public boolean delete(Integer order_id) {
 		return false;
+	}
+
+	@Override
+	public OrderVO ecPay(OrderVO orderVO) throws UnsupportedEncodingException {
+		List<OrderSmallVO> orderSmallVOs2 = orderVO.getOrderSmallVO();
+		// 綠界取出商品名稱
+		List<String> productName = orderSmallVOs2.stream().map(e->e.getName()).collect(Collectors.toList());
+		// 為了配合綠界
+		Optional<String> reduce = productName.stream().reduce((String acc, String curr) -> {
+			return acc + "#" + curr;
+		});
+		String name = reduce.get();
+		// 時間
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		String orderDate = sdf.format(new Date());
+		allInOne = new AllInOne("");
+		AioCheckOutALL aCheckOut = new AioCheckOutALL();
+		aCheckOut.setMerchantTradeNo(orderDate + "tga103");
+		aCheckOut.setMerchantTradeDate(orderDate);
+		aCheckOut.setTotalAmount(orderVO.getPrice() + "");
+		aCheckOut.setTradeDesc("test");
+		aCheckOut.setItemName(name);
+		aCheckOut.setClientBackURL("http://localhost:8080/TGA103G1/main.html");
+		aCheckOut.setReturnURL("http://localhost:8080/TGA103G1/main.html");
+		aCheckOut.setNeedExtraPaidInfo("N");
+
+		String checkValue = EcpayFunction.genCheckMacValue("pwFHCqoQZGmho4w6", "EkRm7iFT261dpevs", aCheckOut);
+		orderVO.setAio(aCheckOut);
+		orderVO.setMessage(checkValue);
+		return orderVO;
 	}
 
 }
